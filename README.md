1fdc91f11b6b40c89cbe47e3bcac179c6701dda5
papix <mail@papix.net>
2016-03-21 07:27:03 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Anego - It's new $module
+
+# SYNOPSIS
+
+    use Anego;
+
+# DESCRIPTION
+
+Anego is ...
+
+# LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+papix <mail@papix.net>
---
+++ lib/Anego.pm
@@ -0,0 +1,39 @@
+package Anego;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Anego - It's new $module
+
+=head1 SYNOPSIS
+
+    use Anego;
+
+=head1 DESCRIPTION
+
+Anego is ...
+
+=head1 LICENSE
+
+Copyright (C) papix.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+papix E<lt>mail@papix.netE<gt>
+
+=cut
+
